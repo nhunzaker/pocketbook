22317c02638d3a0147e4634bf8c2093960febf9f
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-03-13 22:06:51 -0600

    compile

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,6 @@ import Routes from 'Routes'
 let el  = document.getElementById('app')
 let app = new App()
 
-Router.run(Routes, Router.HistoryLocation, (Handler, config) => {
+Router.run(Routes, (Handler, config) => {
   React.render(<Handler flux={ app } { ...config }/>, el)
 })
